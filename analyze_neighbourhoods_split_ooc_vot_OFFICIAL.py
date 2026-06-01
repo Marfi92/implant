17d05e3a3e18f8f8923edac4199aa07afc0496ed
Marjan Firouznia <117081269+Marfi92@users.noreply.github.com>
2026-06-01 09:59:04 +0200

    Update analyze_neighbourhoods_split_ooc_vot_OFFICIAL.py

--- analyze_neighbourhoods_split_ooc_vot_OFFICIAL.py
+++ analyze_neighbourhoods_split_ooc_vot_OFFICIAL.py
@@ -1,6 +1,6 @@
 
 #python analyze_neighbourhoods_split_ooc_vot_OFFICIAL.py /home/abragam23/federatedhealth_20250617/results_nov12_2025/17dc75eb-6f4c-466b-92bc-60882b73c01c/local_test_results/vector_database_FL_global_model_19/lancedb_direct-words_aggregated-dev_dataset_split_seed_3312143636-cosine-128-neighbourhoods/ --make-splits --master_list /home/abragam23/fedhealth_data/Glossary_updated_master.txt --stop_list /home/abragam23/fedhealth_data/stop_list_freq_1.txt --split_output /home/abragam23/fedhealth_data/implant_split_official.json
-
+#python analyze_neighbourhoods_split_ooc_vot_OFFICIAL.py /home/abragam23/federatedhealth_20250617/results_nov12_2025/17dc75eb-6f4c-466b-92bc-60882b73c01c/local_test_results/vector_database_FL_global_model_19/lancedb_direct-words_aggregated-dev_dataset_split_seed_3312143636-cosine-128-neighbourhoods/ --splits-file /home/abragam23/fedhealth_data/implant_split_official.json --official --stop_list /home/abragam23/fedhealth_data/stop_list_freq_1.txt --output-dir /home/abragam23/federatedhealth_20250617/results_nov12_2025/17dc75eb-6f4c-466b-92bc-60882b73c01c/local_test_results/vector_database_FL_global_model_19/analysis_official/ --recalculate
 
 import argparse
 import json
